60f751533de9d83c9c15b8c646024607725b811c
Benjamin Tucker <betucker@ucsd.edu>
2021-04-10 19:51:01 -0700

    clean up

--- worldgen/Site_overlay.ml
+++ worldgen/Site_overlay.ml
@@ -27,6 +27,23 @@ type t_with_canon = t * Canonical_overlay.delta [@@deriving bin_io]
 let overlay =
   Overlay.make_overlay "site" bin_reader_t_with_canon bin_writer_t_with_canon
 
+let filter_and_grab_until_full list ~bag ~target ~f =
+  let rec go list ~unwanted =
+    let count = Bag.length bag in
+    if count >= target then List.rev_append unwanted list
+    else
+      match list with
+      | [] ->
+          List.rev_append unwanted list
+      | elem :: rest -> (
+        match f elem with
+        | Some a ->
+            Bag.add_unit bag a ; go rest ~unwanted
+        | None ->
+            go rest ~unwanted:(elem :: unwanted) )
+  in
+  go list ~unwanted:[]
+
 let prepare () =
   let canon = Canonical_overlay.require () in
   let available_sites =
@@ -37,28 +54,11 @@ let prepare () =
   let entrances = Bag.create () in
   let target_gate_count = 10_000 in
   let gates = Bag.create () in
-  let rec filter_and_grab_until_full list ~unwanted ~bag ~target ~f =
-    let count = Bag.length bag in
-    if count >= target then List.rev_append unwanted list
-    else
-      match list with
-      | [] ->
-          List.rev_append unwanted list
-      | elem :: rest -> (
-        match f elem with
-        | Some a ->
-            Bag.add_unit bag a ;
-            filter_and_grab_until_full rest ~unwanted ~bag ~target ~f
-        | None ->
-            filter_and_grab_until_full rest ~unwanted:(elem :: unwanted) ~bag
-              ~target ~f )
-  in
   available_sites
-  |> filter_and_grab_until_full ~unwanted:[] ~bag:entrances
-       ~target:target_entrance_count ~f:(fun (x, z) ->
-         Entrance_site.prepare ~x ~z )
-  |> filter_and_grab_until_full ~unwanted:[] ~bag:gates
-       ~target:target_gate_count ~f:(fun (x, z) -> Gate_site.prepare ~x ~z)
+  |> filter_and_grab_until_full ~bag:entrances ~target:target_entrance_count
+       ~f:(fun (x, z) -> Entrance_site.prepare ~x ~z)
+  |> filter_and_grab_until_full ~bag:gates ~target:target_gate_count
+       ~f:(fun (x, z) -> Gate_site.prepare ~x ~z)
   |> ignore ;
   let obs = Grid.Mut.create ~side:canon.side Canonical_overlay.Obstacle.Clear in
   let put_obs level ~x ~z =
