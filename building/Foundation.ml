82d5c683a7d72dff827b2f1609897e1221e5f308
Benjamin Tucker <betucker@ucsd.edu>
2021-04-11 15:01:17 -0700

    lay_stairs with max

--- building/Foundation.ml
+++ building/Foundation.ml
@@ -19,11 +19,24 @@ let rectangle_foundation mat ~minx ~maxx ~y ~minz ~maxz =
   let for_m = Mg_util.Range.iter_m ~bind:Building_monad.bind in
   for_m minz maxz (fun z -> for_m minx maxx (fun x -> column_down mat ~x ~y ~z))
 
-let rec lay_stairs stair ~under ~x ~y ~z ~dx ~dz =
+let rec lay_stairs_no_max stair ~under ~x ~y ~z ~dx ~dz =
   let open Building_monad.Let_syntax in
   let%bind here_height = Building_monad.height_at ~x ~z in
   if here_height < y then
     let%bind () = Building_monad.set_block stair ~x ~y ~z in
     let%bind () = column_down under ~x ~y:(y - 1) ~z in
+    lay_stairs_no_max stair ~under ~x:(x + dx) ~y:(y - 1) ~z:(z + dz) ~dx ~dz
+  else return ()
+
+let rec lay_stairs stair ~under ~x ~y ~z ~dx ~dz ~max =
+  let open Building_monad.Let_syntax in
+  let%bind here_height = Building_monad.height_at ~x ~z in
+  if here_height < y then
+    let%bind () =
+      Building_monad.only_prepare (Prepare_monad.fail_if (max <= 0))
+    in
+    let%bind () = Building_monad.set_block stair ~x ~y ~z in
+    let%bind () = column_down under ~x ~y:(y - 1) ~z in
     lay_stairs stair ~under ~x:(x + dx) ~y:(y - 1) ~z:(z + dz) ~dx ~dz
+      ~max:(max - 1)
   else return ()
--- building/Prepare_monad.ml
+++ building/Prepare_monad.ml
@@ -6,7 +6,7 @@ let add_obstacle state ~x ~z = {obstacles= (x, z) :: state.obstacles}
 
 let append_state a b = {obstacles= b.obstacles @ a.obstacles}
 
-type 'a result = Ok of 'a * state | Collision
+type 'a result = Ok of 'a * state | Failed
 
 module T = struct
   type 'a t = state -> Shared.pos -> 'a result
@@ -15,8 +15,8 @@ module T = struct
     match t state pos with
     | Ok (a, a_state) ->
         f a a_state pos
-    | Collision ->
-        Collision
+    | Failed ->
+        Failed
 
   let return a state _pos = Ok (a, state)
 
@@ -42,6 +42,9 @@ let prepare (t : 'a t) ~pos =
   let state = {obstacles= []} in
   t state pos
 
+let fail_if cond : unit t =
+ fun state _pos -> if cond then Failed else Ok ((), state)
+
 let put_obstacle ~x ~z state pos =
   let x, _, z = Shared.apply_pos pos ~x ~y:0 ~z in
   let state = add_obstacle state ~x ~z in
@@ -53,4 +56,4 @@ let collide_obstacle ~x ~z state pos =
   if Overlay.Canon.can_build_on (Grid.get x z canon.obstacles) then
     let state = add_obstacle state ~x ~z in
     Ok ((), state)
-  else Collision
+  else Failed
